dd58fea5d2110dade648a37c946bdc091d23546f
Michaela <michaelameit@seznam.cz>
2024-03-08 11:24:26 +0100

    added classes Animal and Flower

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,17 @@
-public class Animal {
+public abstract class Animal {
+
+    protected String type;
+    protected String name;
+    protected Size size;
+    protected String specialCare;
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", specialCare='" + specialCare + '\'' +
+                '}';
+    }
 }
--- src/Flower.java
+++ src/Flower.java
@@ -1,2 +1,18 @@
-public class Flower {
+public abstract class Flower {
+
+    protected String name;
+    protected double price;
+    protected double neededArea;
+    protected double chanceOfGrowth;
+
+    @Override
+    public String toString() {
+        return "Flower{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", neededArea=" + neededArea +
+                ", chanceOfGrowth=" + chanceOfGrowth +
+                '}';
+    }
+
 }

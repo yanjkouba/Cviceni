837f3362c178cab17b79e28f88eda6d2a11ad029
yanjkouba <logeeeer@gmail.com>
2024-03-08 12:51:59 +0100

    Metoda zasadit

--- src/Farmar.java
+++ src/Farmar.java
@@ -1,4 +1,4 @@
-public class Farmar {
+public class Farmar extends Zahonek{
     private String jmeno = "Jarda";
     private int vek = 99;
     private String gender = "None";
@@ -6,4 +6,7 @@ public class Farmar {
     private Boolean vegan = false;
     private int height = 201;
 
+    public void zasadit(String kytka){
+        farma.add(kytka);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+       Zahonek z = new Zahonek();
+       Farmar jarda = new Farmar();
 
+       jarda.zasadit("Jablecnik");
+        System.out.println(jarda.farma);
     }
 }

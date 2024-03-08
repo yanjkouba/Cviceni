3809e40ac1cf85250552b79c0bfc852e23d44514
yanjkouba <logeeeer@gmail.com>
2024-03-08 12:42:27 +0100

    Vytvoril jsem Jardu.

---
+++ src/Farmar.java
@@ -0,0 +1,9 @@
+public class Farmar {
+    private String jmeno = "Jarda";
+    private int vek = 99;
+    private String gender = "None";
+    private String pronouns = "they/them";
+    private Boolean vegan = false;
+    private int height = 201;
+
+}
